87a3cb663e4ef52e2fe27ca063cc2475f7b2fbe9
Felipe Kunzler <felipe.kunzler@sap.com>
2019-04-14 10:24:08 -0300

    Broadcast YAFIM hashtree

--- src/main/scala/spark/YAFIMHashTree.scala
+++ src/main/scala/spark/YAFIMHashTree.scala
@@ -6,16 +6,20 @@ import sequential.Apriori.Itemset
 import sequential.hashtree.HashTree
 
 // TODO: cache pruned transactionsRDDs?
+/**
+  * Notes:
+  * - Missed broadcast on hash tree. 10s imporvement.
+  */
 class YAFIMHashTree extends YAFIM {
 
   override def filterFrequentItemsets(candidates: List[Itemset], transactionsRDD: RDD[Itemset], minSupport: Int, sc: SparkContext) = {
     if (candidates.nonEmpty) {
-      val items = candidates.flatten.distinct // todo: actually helps, or should just use singletions?
+      val items = candidates.flatten.distinct // todo: actually helps, or should just use singletons?
       val hashTree = new HashTree(candidates, items)
-      sc.broadcast(hashTree)
+      val hashTreeBC = sc.broadcast(hashTree)
 
       val t1 = System.currentTimeMillis()
-      val r = transactionsRDD.flatMap(t => hashTree.findCandidatesForTransaction(t.filter(i => items.contains(i)).sorted))
+      val r = transactionsRDD.flatMap(t => hashTreeBC.value.findCandidatesForTransaction(t.filter(i => items.contains(i)).sorted))
         .map(candidate => (candidate, 1))
         .reduceByKey(_ + _)
         .filter(_._2 >= minSupport)
