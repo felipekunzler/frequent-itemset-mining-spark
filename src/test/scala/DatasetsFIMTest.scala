4a4a3a6913d31fb91d172b6a3832d2aace3b78d7
Felipe Kunzler <felipe.kunzler@sap.com>
2019-04-19 16:31:13 -0300

    Run multiple times for metrics

--- src/test/scala/DatasetsFIMTest.scala
+++ src/test/scala/DatasetsFIMTest.scala
@@ -4,39 +4,75 @@ import sequential.Apriori.Itemset
 import sequential._
 import sequential.fpgrowth.FPGrowth
 import sequential.hashtree.AprioriHashTree
-import spark.{DFPS, YAFIM, YAFIMHashTree}
+import spark.{DFPS, RApriori, YAFIM, YAFIMHashTree}
 
 import scala.collection.mutable
 
 class DatasetsFIMTest extends FunSuite with BeforeAndAfterAll {
 
-  private val fimInstances: List[FIM] = List(new YAFIMHashTree, new DFPS)
+  private val fimInstances = List(
+    (0, () => new Apriori),
+    (1, () => new AprioriHashTree),
+    (1, () => new FPGrowth),
+    (1, () => new YAFIM),
+    (1, () => new YAFIMHashTree),
+    (1, () => new RApriori),
+    (1, () => new DFPS))
 
-  private val executionTimes: mutable.ListBuffer[(String, String, Long)] = mutable.ListBuffer()
+  private val datasets = List(
+    (1, "mushroom.txt", 0.35),
+    (1, "pumsb_star.txt", 0.65),
+    (1, "chess.txt", 0.85),
+    (1, "T10I4D100K.txt", 0.03))
+
+  private val runNTimes = 3
+
+  private val executionTimes: mutable.Map[(String, String), List[Long]] = mutable.LinkedHashMap()
   private val resultsCache: mutable.Map[String, List[Itemset]] = mutable.Map()
 
-  List(("mushroom.txt", 0.35), ("pumsb_star.txt", 0.65), ("chess.txt", 0.85), ("T10I4D100K.txt", 0.03)).foreach(t => {
-    fimInstances.foreach(fim => {
-      val className = fim.getClass.getSimpleName
-      test(s"$className - ${t._1}") {
-        val frequentSets = fim.execute("/datasets/" + t._1, " ", t._2)
-        Util.printItemsets(frequentSets)
+  datasets.filter(_._1 == 1).foreach(t => {
+    for (run <- 1 to runNTimes) {
+      fimInstances.filter(_._1 == 1).map(_._2.apply()).foreach(fim => {
 
-        if (!resultsCache.contains(t._1 + t._2))
-          resultsCache.update(t._1 + t._2, frequentSets)
+        val className = fim.getClass.getSimpleName
+        test(s"$className - ${t._2} - $run") {
+          val frequentSets = fim.execute("/datasets/" + t._2, " ", t._3)
+          Util.printItemsets(frequentSets)
 
-        val expectedItemsets = resultsCache(t._1 + t._2)
-        executionTimes.append((className, s"${t._1}", fim.executionTime))
-        assertItemsetsMatch(expectedItemsets, frequentSets, className)
-      }
-    })
+          if (!resultsCache.contains(t._2 + t._3))
+            resultsCache.update(t._2 + t._3, frequentSets)
+          val expectedItemsets = resultsCache(t._2 + t._3)
+
+          val key = (className, s"${t._2}")
+          val executions = executionTimes.getOrElse(key, List.empty[Long])
+          executionTimes.update(key, executions :+ fim.executionTime)
+
+          assertItemsetsMatch(expectedItemsets, frequentSets, className)
+        }
+      })
+    }
   })
 
   override def afterAll() {
-    val table = Seq("Class", "Dataset", "Execution") +: executionTimes.map(t => {
-      Seq(s" ${t._1} ", s" ${t._2} ", f" ${t._3 / 1000d}%1.2f ")
-    })
-    println("\nExecution times:\n" + Util.Tabulator.format(table))
+    val header = Seq("Class ", "Dataset ") ++ (1 to runNTimes).map(i => s" Run $i ") :+ "Mean "
+    var prevDataset = ""
+    val rows = executionTimes.flatMap(t => {
+      val mean = t._2.sum / runNTimes
+      val r = List(Seq(s" ${t._1._1} ", s" ${t._1._2} ") ++ t._2.map(formatExecution(_)) :+ formatExecution(mean))
+      if (prevDataset != t._1._2 && !prevDataset.isEmpty) {
+        prevDataset = t._1._2
+        1.to(runNTimes + 3, 1).map(_ => "") +: r
+      }
+      else {
+        prevDataset = t._1._2
+        r
+      }
+    }).toSeq
+    println("\nExecution times:\n" + Util.Tabulator.format(header +: rows))
+  }
+
+  def formatExecution(value: Double): String = {
+    f" ${value / 1000d}%1.2f "
   }
 
 }
